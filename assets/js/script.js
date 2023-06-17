866d0fc14a7c2ca47a40487dc65c292c016e44a9
JoseCMessias <josecmessias22@gmail.com>
2023-06-17 10:41:03 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-const horas = document.querySelector('#horas');
-const minutos = document.querySelector('#minutos');
-const segundos = document.querySelector('#segundos');
+const horas = document.querySelector("#horas");
+const minutos = document.querySelector("#minutos");
+const segundos = document.querySelector("#segundos");
 
 setInterval(() => {
-   let date = new Date();
-   let dhoras = date.getHours();
-   let dminutos = date.getMinutes();
-   let dsegundos = date.getSeconds();
+  let date = new Date();
+  let dhoras = date.getHours();
+  let dminutos = date.getMinutes();
+  let dsegundos = date.getSeconds();
 
-   horas.innerHTML = `${formatoDeTempo(dhoras)}`;
-   minutos.innerHTML = `${formatoDeTempo(dminutos)}`;
-   segundos.innerHTML = `${formatoDeTempo(dsegundos)}`;
+  horas.innerHTML = `${formatoDeTempo(dhoras)}`;
+  minutos.innerHTML = `${formatoDeTempo(dminutos)}`;
+  segundos.innerHTML = `${formatoDeTempo(dsegundos)}`;
 });
 
-function formatoDeTempo(time)
-{
-   return time < 10 ? '0' + time : time;
+function formatoDeTempo(tempo) {
+  return tempo < 10 ? "0" + tempo : tempo;
 }
